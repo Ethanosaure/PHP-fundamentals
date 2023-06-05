68adbba06865ea7889ccf55f5fae66c030e6ed6b
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-06-05 16:57:10 +0200

    array 1 exercice

--- arrays/arrays.php
+++ arrays/arrays.php
@@ -8,22 +8,41 @@
     <!-- exercice exemple-->
 <?php
 $family = ['papa', 'maman', 'ptite soeur', 'grande soeur', 'grand frère'];
-print_r($family);
+// print_r($family);
 $recipe = ['frites', 'sauter de poulet au chorizo', "durum", "o'tacos"];
-print_r($recipe);
+// print_r($recipe);
 $movies = ['the hobbit', 'matrix', 'berserk', 'interstellar'];
-print_r($movies[3]);
-?>
-<!-- exercice 1 -->
-<?php
+// print_r($movies[3]);
 $me = array('firstname' => 'Ethan',
 'lastname' => 'Dias Marques',
 'age' => 25,
 'season' => 'winter',
 'soccer' => 'false',
 'favorite_movies' => $movies);
-print_r($me);
+// print_r($me);
+?>
+<br>
+<!-- exercices 1 -->
+<?php
+$me['hobbies'] = ['sports', 'coding', 'video games', 'playing with my dog', 'see my friends'];
+$mother = array('firstname' => 'Sabine',
+'lastname' => 'Sabine',
+'age' => 55,
+'season' => 'summer',
+'soccer' => 'false',
+'favorite_movies' => ['something', 'something', 'something', 'something'],
+'hobbies' => ['sleep', 'take sunbath', 'see family', 'gardening']);
 
+$me['mother'] = $mother;
+
+// echo '<pre>';
+// print_r($me);
+// echo '</pre>';
+
+$print_me = count($me['hobbies']);
+$print_mother = count($mother['hobbies']);
+$total = $print_me + $print_mother;
+print_r("$total total hobbies");
 
 
 
